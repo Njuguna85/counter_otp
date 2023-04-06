c9be7dbd4f71bdc3613a2392ad32e7e7dbf12ebf
Dennis Njuguna <dennix.njuguna85@gmail.com>
2023-04-06 12:20:04 +0300

    cache counter

--- lib/counter.ex
+++ lib/counter.ex
@@ -1,18 +1,67 @@
 defmodule Counter do
-  @moduledoc """
-  Documentation for `Counter`.
-  """
+  use GenServer
 
-  @doc """
-  Hello world.
+  # public APIs
+  # when we invoke start link, it will call init which will return
+  # a tuple of ok and initial_count aka will set the state inside the process
+  def start_link(initial_count) do
+    GenServer.start_link(__MODULE__, initial_count)
+  end
 
-  ## Examples
+  def increment(pid) do
+    GenServer.call(pid, :inc)
+  end
 
-      iex> Counter.hello()
-      :world
+  def decrement(pid) do
+    GenServer.call(pid, :dec)
+  end
 
-  """
-  def hello do
-    :world
+  def current(pid) do
+    GenServer.call(pid, :current)
+  end
+
+  def divide(pid, divisor) do
+    GenServer.call(pid, {:divide, divisor})
+  end
+
+  # callbacks
+
+  # initialize counter
+  # this is a callback function ie wo'nt be called directly
+  # will be called by genserver otp library in response to some event
+  def init(initial_count) do
+    # restore the initial state after a crash
+    initial_count =
+      case Cache.lookup(__MODULE__) do
+        {:ok, count} -> count
+        :error -> initial_count
+      end
+
+    {:ok, initial_count}
+  end
+
+  # handle messages
+  def handle_call(:current, _from, count) do
+    {:reply, count, count}
+  end
+
+  def handle_call(:inc, _from, count) do
+    updated_count = count + 1
+
+    # reply to the process that sent the increment message
+    {:reply, updated_count, updated_count}
+  end
+
+  def handle_call(:dec, _from, count) do
+    updated_count = count - 1
+    {:reply, updated_count, updated_count}
+  end
+
+  def handle_call({:divide, divisor}, _from, count) do
+    {:reply, div(count, divisor), count}
+  end
+
+  def terminate(_reason, count) do
+    Cache.save(__MODULE__, count)
   end
 end
---
+++ lib/counter_supervisor.ex
@@ -0,0 +1,20 @@
+defmodule CounterSupervisor do
+  use Supervisor
+
+  # Client/ public api
+
+  def start_link(init_args) do
+    Supervisor.start_link(__MODULE__, init_args)
+  end
+
+  # Callbacks
+
+  def init(init_args) do
+    children = [
+      Cache,
+      {Counter, init_args}
+    ]
+
+    Supervisor.init(children, strategy: :one_for_one)
+  end
+end
